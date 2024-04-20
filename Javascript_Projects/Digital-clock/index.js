6a8dd82b444e77b83a0195a5ee401a1653654639
esambattupavan25 <esambattupavan25@gmail.c0m>
2024-04-20 11:15:42 +0530

    added

--- Javascript_Projects/Digital-clock/index.js
+++ Javascript_Projects/Digital-clock/index.js
@@ -1,27 +1,29 @@
-const hour=document.getElementById("hour")
-const minutes=document.getElementById("minutes")
-const second=document.getElementById("seconds")
-const ampm=document.getElementById("ampm")
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
 
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
 
-function updateClock(){
-    let h = new Date().getHours()
-    let m = new Date().getMinutes()
-    let s = new Date().getSeconds()
-    let ampm = "AM"
+    if (h > 12) {
+      h = h - 12;
+      ampm = "PM";
+  }
 
-    if(h>12){
-        h = h- 12 
-        ampm ="PM"
-    }
-    hour.innerText=h;
-    minutes.innerText=m;
-    second.innerText=s;
-    ampm,(innerText = ampm);
-    setTimeout(()=>{
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
 
-    },1000)
-
-}
-updateClock()
-
+    hourEl.innerText = h;
+    minuteEl.innerText = m;
+    secondEl.innerText = s;
+    ampmEl.innerText = ampm;
+    setTimeout(() => {
+      updateClock();
+  }, 1000);
+ }
+    updateClock();
